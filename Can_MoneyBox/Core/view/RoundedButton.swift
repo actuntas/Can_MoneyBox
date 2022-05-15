9b4d1bcff1a9fd5587543629433417966eaaafe7
Can Tuntas <76773880+actuntas@users.noreply.github.com>
2022-05-15 16:23:10 +0300

    create LoginButton class and update LoginVC

---
+++ Can_MoneyBox/Core/view/RoundedButton.swift
@@ -0,0 +1,44 @@
+//
+//  RoundedButton.swift
+//  Can_MoneyBox
+//
+//  Created by CAN TUNTAS on 15.05.2022.
+//
+
+import UIKit
+
+/// I'm gonna make this nondesignable in order to show programmatic layout skills and i will be able to use iOS 15's newish configuration API.
+///
+class MMButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    convenience init(backgroundColor: UIColor, title: String) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(.purple, for: .normal)
+        self.setTitleColor(.lightGray, for: .disabled)
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 25
+        layer.borderColor = UIColor.purple.cgColor
+        layer.borderWidth = 1
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
+
--- Can_MoneyBox/Login/LoginVC.swift
+++ Can_MoneyBox/Login/LoginVC.swift
@@ -9,13 +9,47 @@ import UIKit
 
 class LoginVC: UIViewController {
     
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: LoginTextfield!
+    @IBOutlet weak var passwordTextField: LoginTextfield!
     @IBOutlet weak var emailErrorLabel: UILabel!
     @IBOutlet weak var passwordErrorLabel: UILabel!
     
+    private lazy var loginButton: MMButton = { // Can add target here because it's lazy
+        let button = MMButton(backgroundColor: .clear, title: "LOG IN")
+        button.addTarget(self, action: #selector(self.loginPressed), for: .touchUpInside)
+        button.isEnabled = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        view.addSubview(loginButton)
+        layoutLoginButton()
+    }
+    
+    private func layoutLoginButton() {
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalTo: passwordErrorLabel.bottomAnchor, constant: 40),
+            loginButton.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            loginButton.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc private func loginPressed() {
+        print("pressed")
+    }
+
+}
+
+extension LoginVC: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
         
     }
 }
