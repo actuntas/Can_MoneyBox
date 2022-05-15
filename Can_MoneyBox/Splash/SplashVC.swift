27515027eef00acb4b6199a97d31b47053af68fc
Can Tuntas <76773880+actuntas@users.noreply.github.com>
2022-05-15 15:25:23 +0300

    start building splashVC

---
+++ Can_MoneyBox/Splash/SplashVC.swift
@@ -0,0 +1,16 @@
+//
+//  SplashVC.swift
+//  Can_MoneyBox
+//
+//  Created by CAN TUNTAS on 15.05.2022.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+}
