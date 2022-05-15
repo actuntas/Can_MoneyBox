4cf434e413f38de135ca2cb3d191aab7b43e53a3
Can Tuntas <76773880+actuntas@users.noreply.github.com>
2022-05-15 04:22:15 +0300

    Initial Commit

---
+++ Can_MoneyBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Can_MoneyBox
+//
+//  Created by CAN TUNTAS on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
