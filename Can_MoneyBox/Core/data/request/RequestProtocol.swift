345a9008c264ad8c5914e9e597831a9911af30f2
Can Tuntas <76773880+actuntas@users.noreply.github.com>
2022-05-15 06:06:57 +0300

    update request protocol

--- Can_MoneyBox/Core/data/request/RequestProtocol.swift
+++ Can_MoneyBox/Core/data/request/RequestProtocol.swift
@@ -63,4 +63,11 @@ extension RequestProtocol {
     
 }
 
+extension RequestProtocol where Response: Decodable {
+    func decode(_ data: Data) throws -> Response {
+        let decoder = JSONDecoder()
+        return try decoder.decode(Response.self, from: data)
+    }
+}
+
 
