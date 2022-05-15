f9e55e1369d62f6dcdec81db19d87ef2eb335dc7
Can Tuntas <76773880+actuntas@users.noreply.github.com>
2022-05-15 04:56:41 +0300

    create networking skeleton

---
+++ Can_MoneyBox/Core/data/request/RequestProtocol.swift
@@ -0,0 +1,70 @@
+//
+//  RequestProtocol.swift
+//  Can_MoneyBox
+//
+//  Created by CAN TUNTAS on 15.05.2022.
+//
+
+import Foundation
+
+protocol RequestProtocol {
+    
+    var path: String { set get }
+    var httpMethod: RequestMethod { set get }
+    var headers: Dictionary<String,String> { set get }
+    var httpBody: [String:Any] { set get } //body of url when using post methot
+    var bearerToken:String? { set get }
+    func decode<T:Decodable>(_ data: Data) throws -> T?
+}
+
+extension RequestProtocol {
+    
+    var baseURL: String {
+        APIConstants.host
+    }
+    
+    var path:String? {
+        APIConstants.loginPath
+    }
+    
+    var headers: [String : String] {
+        APIConstants.loginHeader
+    }
+    
+    var bearerToken:String? {
+        nil
+    }
+    
+    func createURLRequest() throws -> URLRequest {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = baseURL
+        components.path = path
+        
+        guard let url = components.url else { throw NetworkError.invalidURL }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = httpMethod.rawValue
+        
+        if !headers.isEmpty {
+            urlRequest.allHTTPHeaderFields = headers
+        }
+        if bearerToken != nil {
+            urlRequest.setValue(bearerToken, forHTTPHeaderField: "Authorization")
+        }
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        if !httpBody.isEmpty {
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: httpBody)
+        }
+        
+        return urlRequest
+    }
+    
+    func decode<T:Decodable>(_ data: Data) throws -> T? {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T?.self, from: data)
+    }
+    
+}
+
+
