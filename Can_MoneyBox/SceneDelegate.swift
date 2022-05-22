fe55053309a7b571275be48e424f66cfa1744b0d
Can Tuntas <76773880+actuntas@users.noreply.github.com>
2022-05-22 22:31:36 +0300

    remove iOS version check (already minimum iOS 13.0)

--- Can_MoneyBox/SceneDelegate.swift
+++ Can_MoneyBox/SceneDelegate.swift
@@ -14,9 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        if #available(iOS 13.0, *) {
-            window?.overrideUserInterfaceStyle = .dark
-        }
+        window?.overrideUserInterfaceStyle = .dark
         
         guard let _ = (scene as? UIWindowScene) else { return }
     }
